1ec90572ce876b5c549c75562f870c901aaeb2a3
Matt Mittelsted <mattmittelsted@gmail.com>
2020-06-11 12:11:53 -0600

    add v2t-chat to projects
    Also refactor code slightly

--- main.js
+++ main.js
@@ -1,35 +1,36 @@
-window.onload = function() {
-    document.getElementById("letterm").className = "name-cap mclass";
-    document.getElementById("lettera").className = "name aclass";
-    document.getElementById("lettert1").className = "name t1class";
-    document.getElementById("lettert2").className = "name t2class";
-    document.getElementById("letterh").className = "name hclass";
-    document.getElementById("lettere").className = "name eclass";
-    document.getElementById("letterw").className = "name wclass";
-    document.getElementById("letterm2").className = "name-cap m2class";
-    document.getElementById("letteri").className = "name iclass";
-    document.getElementById("lettert3").className = "name t3class";
-    document.getElementById("lettert4").className = "name t4class";
-    document.getElementById("lettere2").className = "name e2class";
-    document.getElementById("letterl").className = "name lclass";
-    document.getElementById("letters").className = "name sclass";
-    document.getElementById("lettert5").className = "name t5class";
-    document.getElementById("lettere3").className = "name e3class";
-    document.getElementById("letterd").className = "name dclass";
+window.onload = function () {
+  document.getElementById("letterm").className = "name-cap mclass";
+  document.getElementById("lettera").className = "name aclass";
+  document.getElementById("lettert1").className = "name t1class";
+  document.getElementById("lettert2").className = "name t2class";
+  document.getElementById("letterh").className = "name hclass";
+  document.getElementById("lettere").className = "name eclass";
+  document.getElementById("letterw").className = "name wclass";
+  document.getElementById("letterm2").className = "name-cap m2class";
+  document.getElementById("letteri").className = "name iclass";
+  document.getElementById("lettert3").className = "name t3class";
+  document.getElementById("lettert4").className = "name t4class";
+  document.getElementById("lettere2").className = "name e2class";
+  document.getElementById("letterl").className = "name lclass";
+  document.getElementById("letters").className = "name sclass";
+  document.getElementById("lettert5").className = "name t5class";
+  document.getElementById("lettere3").className = "name e3class";
+  document.getElementById("letterd").className = "name dclass";
 }
 
-$(function() {
-    $(".left").addClass("appearright");
-    $(".left").removeClass("invisible");
-    $(".left").removeClass("appearrightrev");
-    $(".right").addClass("appearleft");
-    $(".right").removeClass("invisible");
-    $(".right").removeClass("appearleftrev");
+$(function () {
+  $(".left").addClass("appearright");
+  $(".left").removeClass("invisible");
+  $(".left").removeClass("appearrightrev");
+  $(".right").addClass("appearleft");
+  $(".right").removeClass("invisible");
+  $(".right").removeClass("appearleftrev");
 });
 
 
-window.onscroll = function() {
+window.onscroll = function () {
   moveName();
+  v2tChat();
   thaiBhan();
   yelpCamp();
   acme();
@@ -41,7 +42,7 @@ window.onscroll = function() {
 
 // Appearing name
 function moveName() {
-  if (document.documentElement.scrollTop > 400) {
+  if (document.documentElement.scrollTop >= 400) {
     document.getElementById("letterm").className = "name-cap mclassrev";
     document.getElementById("lettera").className = "name aclassrev";
     document.getElementById("lettert1").className = "name t1classrev";
@@ -80,8 +81,8 @@ function moveName() {
   }
 }
 
-function logos(){
-  if (document.documentElement.scrollTop < 400) {
+function logos() {
+  if (document.documentElement.scrollTop <= 400) {
     $(".left").addClass("appearright");
     $(".left").removeClass("invisible");
     $(".left").removeClass("appearrightrev");
@@ -103,62 +104,76 @@ function noGit() {
 
 //Functions to bring in projects
 
-function thaiBhan() {
-  if (document.documentElement.scrollTop > 300 && document.documentElement.scrollTop < 1200) {
-    document.getElementById("thaibhan").className = "img-fluid appearright";
-    document.getElementById("thaibhan-desc").className = "pl-5 appearleft" 
-  } else if(document.documentElement.scrollTop < 300 || document.documentElement.scrollTop > 1200) {
-    if (document.getElementById("thaibhan").className === "img-fluid appearright") {
-    document.getElementById("thaibhan").className = "img-fluid appearrightrev";
-     document.getElementById("thaibhan-desc").className = "pl-5 appearleftrev";
+function v2tChat() {
+  if (document.documentElement.scrollTop > 500 && document.documentElement.scrollTop < 1300) {
+    document.getElementById("v2t-chat").className = "img-fluid appearright";
+    document.getElementById("v2t-desc").className = "pl-5 appearleft"
+  } else {
+    if (document.getElementById("v2t-chat").className === "img-fluid appearright") {
+      document.getElementById("v2t-chat").className = "img-fluid appearrightrev";
+      document.getElementById("v2t-desc").className = "pl-5 appearleftrev";
     }
   }
 }
 
-function yelpCamp() {
-  if (document.documentElement.scrollTop > 600 && document.documentElement.scrollTop < 1500) {
-    document.getElementById("yelpcamp").className = "img-fluid appearright";
-    document.getElementById("yelpcamp-desc").className = "pl-5 appearleft";
-  } else if(document.documentElement.scrollTop < 600 || document.documentElement.scrollTop > 1500) {
-    if (document.getElementById("yelpcamp").className === "img-fluid appearright") {
-    document.getElementById("yelpcamp").className = "img-fluid appearrightrev";
-      document.getElementById("yelpcamp-desc").className = "pl-5 appearleftrev";
-    }
-  }
-}
-
-function acme() {
-  if (document.documentElement.scrollTop > 1000 && document.documentElement.scrollTop < 1900) {
-    document.getElementById("acmeprod").className = "img-fluid appearright";
-    document.getElementById("acmeprod-desc").className = "pl-5 appearleft";
-  } else if(document.documentElement.scrollTop < 1000 || document.documentElement.scrollTop > 1900) {
-    if (document.getElementById("acmeprod").className === "img-fluid appearright") {
-    document.getElementById("acmeprod").className = "img-fluid appearrightrev";
-      document.getElementById("acmeprod-desc").className = "pl-5 appearleftrev";
-    }
-  }
-}
 function cpanel() {
-  if (document.documentElement.scrollTop > 1400 && document.documentElement.scrollTop < 2300) {
+  if (document.documentElement.scrollTop >= 1000 && document.documentElement.scrollTop < 1800) {
     document.getElementById("cpanelprod").className = "img-fluid appearright";
     document.getElementById("cpanel-desc").className = "pl-5 appearleft";
-  } else if(document.documentElement.scrollTop < 1000 || document.documentElement.scrollTop > 1900) {
+  } else {
     if (document.getElementById("cpanelprod").className === "img-fluid appearright") {
-    document.getElementById("cpanelprod").className = "img-fluid appearrightrev";
+      document.getElementById("cpanelprod").className = "img-fluid appearrightrev";
       document.getElementById("cpanel-desc").className = "pl-5 appearleftrev";
     }
   }
 }
 
-//Make scroll down indicator dissapear when bottom is reached
-function dissapear(){
-   if (document.documentElement.scrollTop > 1400){
-     document.getElementById("triangle-down").className = "invisible";
-   }
+function yelpCamp() {
+  if (document.documentElement.scrollTop >= 1400 && document.documentElement.scrollTop < 2200) {
+    document.getElementById("yelpcamp").className = "img-fluid appearright";
+    document.getElementById("yelpcamp-desc").className = "pl-5 appearleft";
+  } else {
+    if (document.getElementById("yelpcamp").className === "img-fluid appearright") {
+      document.getElementById("yelpcamp").className = "img-fluid appearrightrev";
+      document.getElementById("yelpcamp-desc").className = "pl-5 appearleftrev";
+    }
+  }
 }
 
-function footer(){
-  if (document.documentElement.scrollTop > 2200) {
+function thaiBhan() {
+  if (document.documentElement.scrollTop >= 2000 && document.documentElement.scrollTop < 2600) {
+    document.getElementById("thaibhan").className = "img-fluid appearright";
+    document.getElementById("thaibhan-desc").className = "pl-5 appearleft"
+  } else {
+    if (document.getElementById("thaibhan").className === "img-fluid appearright") {
+      document.getElementById("thaibhan").className = "img-fluid appearrightrev";
+      document.getElementById("thaibhan-desc").className = "pl-5 appearleftrev";
+    }
+  }
+}
+
+
+function acme() {
+  if (document.documentElement.scrollTop >= 2200 && document.documentElement.scrollTop < 2800) {
+    document.getElementById("acmeprod").className = "img-fluid appearright";
+    document.getElementById("acmeprod-desc").className = "pl-5 appearleft";
+  } else {
+    if (document.getElementById("acmeprod").className === "img-fluid appearright") {
+      document.getElementById("acmeprod").className = "img-fluid appearrightrev";
+      document.getElementById("acmeprod-desc").className = "pl-5 appearleftrev";
+    }
+  }
+}
+
+//Make scroll down indicator dissapear when bottom is reached
+function dissapear() {
+  if (document.documentElement.scrollTop >= 2200) {
+    document.getElementById("triangle-down").className = "invisible";
+  }
+}
+
+function footer() {
+  if (document.documentElement.scrollTop > 2700) {
     document.getElementById("footer").className = "appeartop";
   } else {
     document.getElementById("footer").className = "appeartoprev";
