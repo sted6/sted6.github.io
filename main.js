1aed3accd4e3498fe6b234544c4fa8cf5dabd86f
Matt Mittelsted <mattmittelsted@gmail.com>
2020-06-11 13:14:59 -0600

    update footer animation trigger point AGAIN

--- main.js
+++ main.js
@@ -173,7 +173,7 @@ function dissapear() {
 }
 
 function footer() {
-  if (document.documentElement.scrollTop >= 1950) {
+  if (document.documentElement.scrollTop >= 2100) {
     document.getElementById("footer").className = "appeartop";
   } else {
     document.getElementById("footer").className = "appeartoprev";
