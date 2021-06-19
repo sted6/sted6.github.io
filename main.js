bf4d1caa74845f35e512e2778a8623d91c1edc07
Matt Mittelsted <mattmittelsted@gmail.com>
2021-06-19 17:22:43 -0600

    Add features to pern messenger, fix onload functions

--- main.js
+++ main.js
@@ -27,29 +27,12 @@ window.onscroll = function () {
   smashSuite();
   dissapear();
   footer();
+  moveName();
 };
 
 // Appearing name
 function moveName() {
-  if (document.documentElement.scrollTop > 400) {
-    document.getElementById("letterm").className = "name-cap mclassrev";
-    document.getElementById("lettera").className = "name aclassrev";
-    document.getElementById("lettert1").className = "name t1classrev";
-    document.getElementById("lettert2").className = "name t2classrev";
-    document.getElementById("letterh").className = "name hclassrev";
-    document.getElementById("lettere").className = "name eclassrev";
-    document.getElementById("letterw").className = "name wclassrev";
-    document.getElementById("letterm2").className = "name-cap m2classrev";
-    document.getElementById("letteri").className = "name iclassrev";
-    document.getElementById("lettert3").className = "name t3classrev";
-    document.getElementById("lettert4").className = "name t4classrev";
-    document.getElementById("lettere2").className = "name e2classrev";
-    document.getElementById("letterl").className = "name lclassrev";
-    document.getElementById("letters").className = "name sclassrev";
-    document.getElementById("lettert5").className = "name t5classrev";
-    document.getElementById("lettere3").className = "name e3classrev";
-    document.getElementById("letterd").className = "name dclassrev";
-  } else {
+  if (document.documentElement.scrollTop < 400) {
     document.getElementById("letterm").className = "name-cap mclass";
     document.getElementById("lettera").className = "name aclass";
     document.getElementById("lettert1").className = "name t1class";
@@ -67,6 +50,24 @@ function moveName() {
     document.getElementById("lettert5").className = "name t5class";
     document.getElementById("lettere3").className = "name e3class";
     document.getElementById("letterd").className = "name dclass";
+  } else {
+    document.getElementById("letterm").className = "name-cap mclassrev";
+    document.getElementById("lettera").className = "name aclassrev";
+    document.getElementById("lettert1").className = "name t1classrev";
+    document.getElementById("lettert2").className = "name t2classrev";
+    document.getElementById("letterh").className = "name hclassrev";
+    document.getElementById("lettere").className = "name eclassrev";
+    document.getElementById("letterw").className = "name wclassrev";
+    document.getElementById("letterm2").className = "name-cap m2classrev";
+    document.getElementById("letteri").className = "name iclassrev";
+    document.getElementById("lettert3").className = "name t3classrev";
+    document.getElementById("lettert4").className = "name t4classrev";
+    document.getElementById("lettere2").className = "name e2classrev";
+    document.getElementById("letterl").className = "name lclassrev";
+    document.getElementById("letters").className = "name sclassrev";
+    document.getElementById("lettert5").className = "name t5classrev";
+    document.getElementById("lettere3").className = "name e3classrev";
+    document.getElementById("letterd").className = "name dclassrev";
   }
 }
 
